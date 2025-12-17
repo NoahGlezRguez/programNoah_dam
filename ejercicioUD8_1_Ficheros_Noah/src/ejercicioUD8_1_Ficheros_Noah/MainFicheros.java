677585a689c0ff417798af284b33e67fb6ad3adc
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-17 13:56:35 +0100

    enunciado (con dudas)
    Hay algunas cosas que no entiendo pero la idea es hoy analizarlo en clase y preguntar, etc

--- ejercicioUD8_1_Ficheros_Noah/src/ejercicioUD8_1_Ficheros_Noah/MainFicheros.java
+++ ejercicioUD8_1_Ficheros_Noah/src/ejercicioUD8_1_Ficheros_Noah/MainFicheros.java
@@ -6,7 +6,29 @@ public class MainFicheros {
 
 	public static void main(String[] args) {
 		
-
+		/*
+		 * Mostrar menu en consola
+		 * 1.- Cargar Mensajes
+		 * 2.- Guardar Mensajes
+		 * 3.- Añadir mensaje
+		 * 4.- Imprimir mensaje
+		 * 5.- Salir
+		 * 
+		 * 1.- con arrays y objetos
+		 * 		se mostrara numero total de mensajes cargados (se pueden cargar cada vez mas sin guardar ni nada)
+		 * 
+		 * 2.- se guardan los ya cargados en el fichero mensajes.txt
+		 * 		se mostrara mensaje de "Los mensajes en memoria han sido guardados en el fichero mensajes.txt"
+		 * 
+		 * 3.- se pediran los siguientes datos:
+		 * 		fecha, hora, de, para, asunto, contenido
+		 * 		el mensaje se añade usando un objeto, en la memoria
+		 * 
+		 * 4.- imprimir los mensajes en una tabla con la siguiente estructura:
+		 * 		de	para		fecha	hora		asunto	contenido
+		 * 		Si no hubiera mas mensajes, se mostrara "No hay mensajes que mostrar"
+		 * 
+		 * */
 	}
 
 }
