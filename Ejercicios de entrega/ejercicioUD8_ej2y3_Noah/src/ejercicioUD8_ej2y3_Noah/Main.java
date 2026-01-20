8ff098b2ddfd6b43b8a4364618f59fe958291884
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 13:06:08 +0100

    sin mas

---
+++ Ejercicios de entrega/ejercicioUD8_ej2y3_Noah/src/ejercicioUD8_ej2y3_Noah/Main.java
@@ -0,0 +1,10 @@
+package ejercicioUD8_ej2y3_Noah;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Ejercicios de entrega/ejercicioUD8_ej2y3_Noah/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ejercicioUD8_ej2y3_Noah {
+}
