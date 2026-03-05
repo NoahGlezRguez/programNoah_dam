3568b5ff0c376bc3bb14bd7c75cc37fc34fb6402
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-03-05 21:03:37 +0100

    esqueleto ej5

---
+++ Ejercicio_4_3_Noah/src/ejercicio5/MainEj5.java
@@ -0,0 +1,13 @@
+package ejercicio5;
+
+public class MainEj5 {
+
+	public static void main(String[] args) {
+		try {
+			Ventana5 frame = new Ventana5();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
