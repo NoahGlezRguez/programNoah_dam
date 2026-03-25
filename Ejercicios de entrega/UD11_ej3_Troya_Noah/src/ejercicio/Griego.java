9c1568e447b5f0d650fe54248408f7817d2dc162
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-03-26 00:57:00 +0100

    parte 3.1 terminada

--- Ejercicios de entrega/UD11_ej3_Troya_Noah/src/ejercicio/Griego.java
+++ Ejercicios de entrega/UD11_ej3_Troya_Noah/src/ejercicio/Griego.java
@@ -2,4 +2,37 @@ package ejercicio;
 
 public class Griego extends Guerrero {
 
+	public Griego(String nombre, int edad, int fuerza) {
+		super(nombre, edad, fuerza);
+		
+	}
+
+	public Griego() {
+		super("GriegoX", 20, 8);
+	}
+	
+	@Override
+	public boolean retirarse() {
+		
+		System.out.print(toString() + "RETIRADA");	
+
+		return false;
+	}
+
+	@Override
+	public String toString() {
+
+		String datos = String.format("Griego: %s\nEdad:%s   Fuerza:%s\n", getNombre(),
+				getEdad(), getFuerza());
+		
+		if (super.isHerido()) {
+			datos += "Está herido ⇾ ";
+		}
+		else {
+			datos += "No está herido ⇾ ";
+		}
+		
+		return (datos);
+	}
+	
 }
